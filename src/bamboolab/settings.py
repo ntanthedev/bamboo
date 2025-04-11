7b31040babce1e00b5a22590cc0c777d2411a08c
iotran207 <lthlht2007@gmail.com>
2025-04-11 23:40:13 +0700

    ập

--- src/bamboolab/settings.py
+++ src/bamboolab/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'USER': os.environ.get('POSTGRES_USER', 'postgres'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'changeme'),
         'HOST': os.environ.get('POSTGRES_HOST', 'db'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'PORT': os.environ.get('POSTGRES_PORT', '5452'),
     }
 }
 
